a171ba9692f8d3f27e559c5cd15a2122a8d04bca
AmeyaBlueappIOT <sireesha.t@billionapps.co>
2016-06-21 17:48:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -133,9 +133,13 @@ app.directive('liquidTank', function ($parse, $http) {
         }
         scope.totalSteps = scope.steps[_.random(scope.steps.length-1)];
 		console.log(scope.totalSteps);
-		console.log("current value in dir----"+window.api.currentValue);
+		console.log("current value in dir----"+window.api.currentValue+5);
         scope.filledSteps = (window.api.currentValue/16)*10;//_.random(1,scope.totalSteps-1);
-		window.api.updateUI();
+	
+	window.api.updateUI = function(){
+        scope.draw();
+        };
+	
         console.log(scope.filledSteps);
         var fillPct = scope.filledSteps/scope.totalSteps;
 		console.log(fillPct);
